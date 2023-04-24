379583a720bd2fb08d09ad97afd8b63710182388
TimurZairov <zairovne@gmail.com>
2023-04-24 15:25:25 +0500

    EditProfileScreen => React Hook Form

--- src/screens/EditProfileScreen/EditProfileScreen.tsx
+++ src/screens/EditProfileScreen/EditProfileScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./style";
 import {Image, Text, TextInput, View} from "react-native";
+import {useForm, Controller, Control} from "react-hook-form";
 
 import user from "../../assets/data/user.json"
 import colors from "../../theme/color";
@@ -9,31 +10,60 @@ import colors from "../../theme/color";
 interface IInputContainer {
     label: string,
     placeHolder: string,
-    multiline?: boolean
+    multiline?: boolean,
+    control: Control,
+    name: string
 }
 
-const InputContainer = ({label, placeHolder, multiline = false} : IInputContainer) => {
-    return (
-        <View style={styles.inputContainer}>
-            <Text style={styles.label}>{label}</Text>
-            <TextInput placeholder={placeHolder} style={styles.input} value={placeHolder} multiline={multiline}/>
-        </View>
-    )
+const InputContainer = ({label, placeHolder, multiline = false, control, name}: IInputContainer) => {
+
+    return <Controller
+        name={name}
+        control={control}
+        render={({field: {onBlur, value, onChange}}) => {
+            return (
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>{label}</Text>
+                    <TextInput placeholder={placeHolder}
+                               style={styles.input}
+                               placeholderTextColor={colors.lightGrey}
+                               multiline={multiline}
+                               onChangeText={onChange}
+                               value={value}
+                               onBlur={onBlur}
+                    />
+                </View>
+            )
+        }
+        }
+    />
+
 }
 
 
 const EditProfileScreen = () => {
+
+    const {control, handleSubmit} = useForm();
+
+    const onSubmit = (data: {}) => console.log(data);
+
+    const changePhotoHandler = () => {
+        console.warn("photo")
+    }
+
+
     return (
         <View style={styles.container}>
             <View style={styles.userContainer}>
-                <Image source={{uri: user.image}} style={styles.userImage} />
-                <Text style={styles.changeText}>Change Profile photo</Text>
+                <Image source={{uri: user.image}} style={styles.userImage}/>
+                <Text style={styles.changeText} onPress={changePhotoHandler}>Change Profile photo</Text>
             </View>
 
-            <InputContainer label="Name"  placeHolder={user.name}/>
-            <InputContainer label="USerName"  placeHolder={user.username}/>
-            <InputContainer label="Website"  placeHolder={""}/>
-            <InputContainer label="Bio"  placeHolder={user.bio} multiline/>
+            <InputContainer label="Name" placeHolder={"Name"} control={control} name={"name"}/>
+            <InputContainer label="USerName" placeHolder={"USerName"} control={control} name={"username"}/>
+            <InputContainer label="Website" placeHolder={"Website"} control={control} name={"website"}/>
+            <InputContainer label="Bio" placeHolder={"Bio"} multiline control={control} name={"bio"}/>
+            <Text style={[styles.changeText, {textAlign: "center"}]} onPress={handleSubmit(onSubmit)}>Submit</Text>
 
         </View>
     )
